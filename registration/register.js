5e2e295f4bcccbe2f4ce98d0ffc9cb86550d3330
Laren Spear <lcspear@cs.utexas.edu>
2021-04-04 18:23:04 -0500

    Signup page

---
+++ registration/register.js
@@ -0,0 +1,43 @@
+document.getElementById("validate").addEventListener("click",valid)
+
+function valid(){
+
+var formElements = document.forms.validator;
+var username = formElements[0].value.toString();
+var password = formElements[1].value.toString();
+var repeat = formElements[2].value.toString();
+var email = formElements[3].value.toString();
+var username_good = false;
+var password_good = false;
+var email_good = false;
+var alpha_regex = /^[a-zA-Z0-9]+$/
+var username_regex = /^[^\d][A-Za-z0-9]+.{4,8}$/ 
+var password_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,10}$/
+var email_regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+if (username_regex.test(username)) {
+    username_good = true;
+    }
+
+if (alpha_regex.test(username) == false){
+    username_good = false;
+    }
+
+if (password_regex.test(password) && password === repeat) {
+    password_good = true;
+    }
+
+if (password_good == false || username_good == false) {
+    alert("Invalid Username or Password")
+} 
+
+if (email_regex.test(email)) {
+    email_good = true;
+}
+
+if (email_good == false) {
+    alert("Invalid Email")
+}   else {
+    document.getElementById('validator').submit()
+}
+}

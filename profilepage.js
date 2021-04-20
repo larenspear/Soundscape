b737fc82bbccf61e743d2f88ff4be3acbabc39e4
aaiovinoo <80013940+aaiovinoo@users.noreply.github.com>
2021-04-20 07:13:26 +0200

    Update profilepage.js
    fixed a bug

--- profilepage.js
+++ profilepage.js
@@ -2,7 +2,7 @@ function loadImages() {
   console.log("loadingimages")
   // we should probably use a multidiminsional dictionary instead of these three arrays, but now it works
 
-  var reviews = new Array("I can’t wait for the month of May! When everyone gets their vaccines, we can start partying again. This really captures the chaotic energy I’ve been feeling. Definitely check this album out.", "An underrated album from Led Zeppelin. All of their standout hits like Stairway to Heaven and Kashmir are on other albums. But the raw sound Presence is really inspiring.", "Of all the people to marry Elon Musk - why? Great album though. It hits the spot when I want powerful pop with strong vocals.", "Great studying music. Way better than lo-fi hip hop beats to relax and study to. This is almost like sound-art, but actually musical instead of just an art piece. The nature that goes along with the YouTube videos makes me feel like I’m out in the mountains.", "RIP Sophie. That is all. The world will never be the same.", "Great mix of r&b and pop. I love her British accent! Her voice is just so beautiful and soulful, whether it’s a ballad or upbeat. ");
+  var reviews = new Array("I canâ€™t wait for the month of May! When everyone gets their vaccines, we can start partying again. This really captures the chaotic energy Iâ€™ve been feeling. Definitely check this album out.", "An underrated album from Led Zeppelin. All of their standout hits like Stairway to Heaven and Kashmir are on other albums. But the raw sound Presence is really inspiring.", "Of all the people to marry Elon Musk - why? Great album though. It hits the spot when I want powerful pop with strong vocals.", "Great studying music. Way better than lo-fi hip hop beats to relax and study to. This is almost like sound-art, but actually musical instead of just an art piece. The nature that goes along with the YouTube videos makes me feel like Iâ€™m out in the mountains.", "RIP Sophie. That is all. The world will never be the same.", "Great mix of r&b and pop. I love her British accent! Her voice is just so beautiful and soulful, whether itâ€™s a ballad or upbeat. ");
 
   var links = new Array("https://open.spotify.com/album/0f3AzoDpzsFoSMe6EB90RY", "https://open.spotify.com/album/6vSiY2OVanKKforfEOT2PD", "https://open.spotify.com/album/7J84ixPVFehy6FcLk8rhk3", "https://open.spotify.com/album/25TwYopjoWFodq0fuJd0Vj", "https://open.spotify.com/playlist/4M9B459P0MBf8phz8sAwRW", "https://open.spotify.com/album/0DriDL7OcMeMENJWAElSYL")
 
@@ -73,13 +73,14 @@ function loadImages() {
     like.appendChild(likeIcon)
     like.appendChild(likeNum)
     tr.appendChild(comment)
-    comment.appendChild(commentIcon)
+    comment.appendChild(link)
+	  link.appendChild(commentIcon)
     comment.appendChild(commentNum)
     tr.appendChild(view)
     view.appendChild(viewIcon)
     view.appendChild(viewNum)
     
-    likeIcon.onclick = countLikes(likeIcon.nextSibling) //not working for some reason?
+   // likeIcon.onclick = countLikes(likeIcon.nextSibling) //not working for some reason?
     
     review = document.createTextNode(reviews[idx]);
     document.getElementById(id).appendChild(review)

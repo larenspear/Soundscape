ca6ab4ebe3ae62d859f3462b1909ba5b48f8c892
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-04 22:33:45 -0500

    Database credentials debugging

--- registration/register.php
+++ registration/register.php
@@ -1,11 +1,21 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
+
+echo $server;
+echo $username;
+echo $password;
+echo $db;
+echo $url;
 
 $mysqli = new mysqli($server, $username, $password, $db);
 
